1130e642f97c8c923ed0375e47c4059ec77241d0
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-04 07:25:00 -0700

    formatting cleanup and stripping of score text to avoide issues with html formating

--- pool_scraping/pool_scraping.py
+++ pool_scraping/pool_scraping.py
@@ -9,7 +9,8 @@ def get_pool_data(html_filename):
     (3) an boolean array indicating winners for each bout and (4) array of scores for each bout
     """
     html_pool = open(html_filename)
-    pool_soup = BeautifulSoup(html_pool, 'html.parser') # caution: contains chlorine!
+    # caution: contains chlorine!
+    pool_soup = BeautifulSoup(html_pool, 'html.parser')
 
     athlete_name_list = []
     athlete_ID_list = []
@@ -17,25 +18,21 @@ def get_pool_data(html_filename):
     for athlete in pool_soup.find_all('a'):
         link = athlete.get('href')
         link_pieces = link.split("/")
-        # print("Athlete Name: {name:<25} Athlete ID: {id}".format(
-        #     name=athlete.get_text(), id=link_pieces[2]))
         athlete_name_list.append(athlete.get_text())
         athlete_ID_list.append(link_pieces[2])
 
     pool_size = len(athlete_name_list)
-    winners_array = np.zeros((pool_size,pool_size),dtype=int)
-    score_array = np.zeros((pool_size,pool_size),dtype=int)
+    winners_array = np.zeros((pool_size, pool_size), dtype=int)
+    score_array = np.zeros((pool_size, pool_size), dtype=int)
 
-    for idx, entry in enumerate(pool_soup.find_all('div',class_="ResultsPool-score")):
-        score = entry.get_text()
-        if(score): 
-            score_pieces = score.split("/") #scores are stored in a 'V/5', 'D/2' format
-            if(score_pieces[0]=='V'):
-                winners_array[int(idx/pool_size)][idx % pool_size] = 1
-            score_array[int(idx/pool_size)][idx % pool_size] = score_pieces[1]
-    # print(winners_array)
-    # print(score_array)
-
+    for idx, entry in enumerate(pool_soup.find_all('div', class_="ResultsPool-score")):
+        score = entry.get_text().strip()
+        if(score):
+            # scores are stored in a 'V/5', 'D/2' format
+            score_pieces = score.split("/")
+            print(score_pieces)
+            if score_pieces[0] == 'V':
+                winners_array[idx // pool_size][idx % pool_size] = 1
+            score_array[idx // pool_size][idx % pool_size] = score_pieces[1]
+    
     return athlete_name_list, athlete_ID_list, winners_array, score_array
-
-
