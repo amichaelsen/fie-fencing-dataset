09678658e6fcf554f1c99dfd38d99557c4f7fba5
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-06 15:09:22 -0700

    added docstring to tournamentData

--- tournaments/tournament_data.py
+++ tournaments/tournament_data.py
@@ -10,11 +10,25 @@ class tournamentData:
 
     Attributes:
     -----------
-    
+    unique_ID : str
+        string to uniquely identify event, combines "season" and "competitionId"
+    pools_list : list[poolData]
+        list of poolData objects representing the pool results
+    fencers_dict : dict
+        a dictionary of fencers at the event, indexed by fencer's 'id' with event
+        specific fencer data: "age" and "points_before_event" (for comparison of ranking)
+    url : str
+        The URL (constructed from season and competitionID) used to find event page
+
+    All other attributes are directly pulled from the JSON window._competition object 
+    with the following relabelings:
+        startDate     -> start_date  
+        endDate       -> end_date
+        competitionId -> competition_ID
     '''
     unique_ID : str # this will be season-competition_id, e.g. "2020-771"
     season : int
-    competition_id : int
+    competition_ID : int
     weapon : str # make this an enum eventually 
     gender : str # make this an enum eventually 
     category : str # make this an enum eventually 
@@ -33,7 +47,7 @@ class tournamentData:
         str_rep = "\nTournament Information:\n"
         str_rep +=   "-----------------------\n"
         str_rep += "   Name:  {:<25} (unique ID {:<10})\n".format(self.name, self.unique_ID)
-        str_rep += "   Season:  {:<10}  Competition ID:  {:<5}\n".format(self.season, self.competition_id)
+        str_rep += "   Season:  {:<10}  Competition ID:  {:<5}\n".format(self.season, self.competition_ID)
         str_rep += "   Weapon:  {:<10}  Gender:          {:<5}\n".format(self.weapon, self.gender)
         str_rep += "   Country: {:<10}  TimeZone:        {:<5}\n".format(self.country, self.timezone)
         str_rep += "   Start:   {:<10}  End:             {:<5}\n".format(self.start_date, self.end_date)
