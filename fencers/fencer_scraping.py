dfb9b4ae06f7ea4ac30b07f0950064a5a780ba9c
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-06 21:55:40 -0700

    finished fencer_scraping method

--- fencers/fencer_scraping.py
+++ fencers/fencer_scraping.py
@@ -1,5 +1,5 @@
-from dataframe_columns import FENCERS_DF_COLS
 import requests
+import json
 from bs4 import BeautifulSoup
 
 
@@ -15,10 +15,21 @@ def get_fencer_info_from_ID(fencer_ID):
     name_tag = soup.find('h1', class_='AthleteHero-fencerName')
     fencer_name = name_tag.get_text()
 
-    # h1 class="AthleteHero-fencerName
-    nationality_tag = soup.find('h1', class_='AthleteHero-fencerName')
-    fencer_name = name_tag.get_text()
-
+    # window._tabOpponents = [{"date":"2021-04-10",
+    #                           "fencer1":{"id":"52027","name":"PARK Faith","nationality":"USA","isWinner":true,"score":"5"},
+    #                           "fencer2":{"id":49302,"name":"CARDOSO Elisabete","nationality":"POR","isWinner":false,"score":"2"},"competition":"Championnats du monde juniors-cadets","season":"2021","competitionId":"235","city":"Le Caire"},
+    script = next(soup.find('script', id="js-single-athlete").children)
+    # each variable window._XXXX is ';' separated and window._tabOpponents contains data that includes fencer nationality
+    var_list = script.split(';')
+    # get window._tabOpponents Data
+    tabOpp_var_name = "window._tabOpponents "
+    tabOpp_string = [text.strip() for text in var_list if
+                     text.strip().startswith(tabOpp_var_name)][0]
+    fencer_data = json.loads(tabOpp_string.split(" = ")[1])[0]['fencer1']
+    if int(fencer_data['id']) == fencer_ID:
+        nationality = fencer_data['nationality']
+    else:
+        nationality = ""
     info_div = soup.find('div', class_="ProfileInfo")
     # info_div has 6 sub-divs for example
     #     <div class="ProfileInfo Container Container--wider">
@@ -47,8 +58,7 @@ def get_fencer_info_from_ID(fencer_ID):
             hand = list(info_item.children)[1].get_text()
         elif(info_item.get_text().startswith('Age')):
             age = list(info_item.children)[1].get_text()
+        elif(info_item.get_text().startswith('Rank')):
+            rank = list(info_item.children)[1].get_text()
 
-    for data in [fencer_url, weapon, hand, points]:
-        print("Info: {} (type = {})".format(data, type(data)))
-
-    return {'id': fencer_ID, 'name': fencer_name, 'url': fencer_url, 'hand': hand, 'weapon': weapon, 'points': points, 'age': age}
+    return {'id': fencer_ID, 'name': fencer_name, 'nationality': nationality, 'url': fencer_url, 'hand': hand, 'weapon': weapon, 'points': points, 'rank': rank, 'age': age}
--- fencers/testing_fencer_scraping.py
+++ fencers/testing_fencer_scraping.py
@@ -1,4 +1,7 @@
-from fencers.fencer_scraping import get_fencer_info_from_url
+from fencers.fencer_scraping import get_fencer_info_from_ID
 
-url = "https://fie.org/athletes/43803"
-get_fencer_info_from_url(url)
+
+fencer_IDs = [43803, 52027]
+for fencer_ID in fencer_IDs:
+    fencer_dict = get_fencer_info_from_ID(fencer_ID)
+    print(fencer_dict)
