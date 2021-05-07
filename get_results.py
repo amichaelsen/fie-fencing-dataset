a6b7e55cee9b4318d27320936f0991288ebf0763
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-06 21:39:31 -0700

    added print status on get results script, simplified pd.Dataframe cleanup, added script for filling fencer data

--- get_results.py
+++ get_results.py
@@ -4,6 +4,12 @@ from dataframe_columns import BOUTS_DF_COLS, TOURNAMENTS_DF_COLS, FENCERS_DF_COL
 from tournaments.tournament_scraping import create_tournament_data_from_url, compile_bout_dataframe_from_tournament_data
 from tournaments.tournament_data import TournamentData
 
+
+# ---------------------------------------------------------------
+# INITIALIZE DATAFRAMES AND URLS
+# ---------------------------------------------------------------
+
+print("Preparing to process tournament data...", end="")
 list_of_urls = ['https://fie.org/competitions/2020/771',
                 'https://fie.org/competitions/2021/1070',
                 'https://fie.org/competitions/2021/92']
@@ -13,7 +19,15 @@ fencers_dataframe = pd.DataFrame(columns=FENCERS_DF_COLS)
 tournaments_dataframe = pd.DataFrame(columns=TOURNAMENTS_DF_COLS)
 bouts_dataframe = pd.DataFrame(columns=BOUTS_DF_COLS)
 
+# temporary list to store IDs before looking up fencer data
 fencer_ID_list = []
+print(" Done!")
+
+
+# ---------------------------------------------------------------
+# PROCESS TOURNAMENTS INDIVIDUALLY
+# ---------------------------------------------------------------
+print("Processing {} tournaments: ".format(len(list_of_urls)), end="")
 
 for tournament_url in list_of_urls:
     # process data from the event
@@ -23,50 +37,74 @@ for tournament_url in list_of_urls:
     tournament_info_dict = tournament_data.create_tournament_dict()
     tournament_fencer_ID_list = list(tournament_data.fencers_dict.keys())
 
-    # print some info here about each tournament (# of athletes, pools, total bout count)
-    print("\nTournament: {} (ID {})\n".format(
-        tournament_info_dict['name'], tournament_info_dict['unique_ID']))
-    print("Total Fencer Count: {}".format(len(tournament_fencer_ID_list)))
-    print("Number of Bouts: {}".format(tournament_bout_dataframe.shape[1]))
+    # # print some info here about each tournament (# of athletes, pools, total bout count)
+    # print("\nTournament: {} (ID {})\n".format(
+    #    tournament_info_dict['name'], tournament_info_dict['unique_ID']))
+    # print("Total Fencer Count: {}".format(len(tournament_fencer_ID_list)))
+    # print("Number of Bouts: {}".format(tournament_bout_dataframe.shape[1]))
 
     # add tournament data to overall dataframes/lists
     fencer_ID_list = list(set(fencer_ID_list+tournament_fencer_ID_list))
     bouts_dataframe = bouts_dataframe.append(tournament_bout_dataframe)
     tournaments_dataframe = tournaments_dataframe.append(
         tournament_info_dict, ignore_index=True)
+    print(".", end="")
+
+print(" Done!")
+# ---------------------------------------------------------------
+# PROCESS INDIVIDUAL FENCER DATA
+# ---------------------------------------------------------------
+print("Processing {} fencers: ".format(len(fencer_ID_list)), end="")
+
+for fencer_ID in fencer_ID_list:
+    fencer_info_dict = get_fencer_info_from_url(fencer_ID)
+    fencers_dataframe.append(fencer_info_dict, ignore_index=True)
+    print(".", end="")
+
+print(" Done!")
+
+# ---------------------------------------------------------------
+# CLEAN UP DATAFRAMES
+# ---------------------------------------------------------------
+print("Cleaning up dataframes...", end="")
 
 
-def expand_weapon(wpn_str):
-    weapon_dict = {'E': "Epee", "F": "Foil", "S": "Saber"}
-    return weapon_dict.get(wpn_str, wpn_str)
-
-
-def expand_gender(gdr_str):
-    gender_dict = {"M": "Mens", "F": "Womens"}
-    return gender_dict.get(gdr_str, gdr_str)
-
-
-def expand_category(cat_str):
-    category_dict = {"J": "Junior", "C": "Cadet",
-                     "S": "Senior", "V": "Veterans"}
-    return category_dict.get(cat_str, cat_str)
-
+# expand labels for 'weapon', 'gender' and 'category' in the tournament dataframe
+weapon_dict = {'E': "Epee", "F": "Foil", "S": "Saber"}
+gender_dict = {"M": "Mens", "F": "Womens"}
+category_dict = {"J": "Junior", "C": "Cadet",
+                 "S": "Senior", "V": "Veterans"}
 
 tournaments_dataframe['weapon'] = tournaments_dataframe['weapon'].map(
-    expand_weapon)
-
+    weapon_dict)
 tournaments_dataframe['gender'] = tournaments_dataframe['gender'].map(
-    expand_gender)
-
+    gender_dict)
 tournaments_dataframe['category'] = tournaments_dataframe['category'].map(
-    expand_category)
+    category_dict)
 
+tournaments_dataframe['start_date'] = pd.to_datetime(
+    tournaments_dataframe['start_date'])
+tournaments_dataframe['end_date'] = pd.to_datetime(
+    tournaments_dataframe['end_date'])
+bouts_dataframe['date'] = pd.to_datetime(bouts_dataframe['date'])
+
+categorical_data = ['weapon', 'gender', 'category']
+for cat in categorical_data:
+    tournaments_dataframe[cat] = tournaments_dataframe[cat].astype('category')
+
+print("Done!")
+# ---------------------------------------------------------------
+# PRINT OUTPUTS TO VERIFY DATA
+# ---------------------------------------------------------------
+
+# Print dataframes (or parts of them) to see output
 
 print(tournaments_dataframe.drop(
     columns=['timezone', 'url', 'end_date']).to_markdown())
-
+# print(tournaments_dataframe.info())
 
 bout_count = 10
 idx = random.sample(list(bouts_dataframe.index), bout_count)
 print("\nA random selection of {} bouts from list:\n".format(bout_count))
 print(bouts_dataframe.loc[idx].to_markdown())
+# print(bouts_dataframe.info())
