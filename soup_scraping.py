75a22be0fc69275c0605298f822bcee55e7d4e3b
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-11 08:40:27 -0700

    added method for creating http request json params for results search

--- soup_scraping.py
+++ soup_scraping.py
@@ -1,5 +1,6 @@
-import json 
+import json
 from bs4 import BeautifulSoup
+
 
 def get_json_var_from_script(soup, script_id, var_name):
     # each variable window._XXXX is ';' separated
@@ -8,7 +9,14 @@ def get_json_var_from_script(soup, script_id, var_name):
 
     # get var_name Data (may be list or dict)
     var_string = [text.strip() for text in var_list if
-                     text.strip().startswith(var_name)][0]
+                  text.strip().startswith(var_name)][0]
     json_variable = json.loads(var_string.split(" = ")[1])
     return json_variable
-    
+
+
+def get_search_params(weapon=[], gender=[], category="", page=1):
+    return {"name": "", "status": "passed",
+            "gender": gender, "weapon": weapon,
+            "type": ["i"], "season": "-1", "level": category,
+            "competitionCategory": "", "fromDate": "",
+            "toDate": "", "fetchPage": page}
