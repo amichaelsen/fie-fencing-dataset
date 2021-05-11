102564c43c02754760e2ea9c57576174423b5a21
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-11 11:27:54 -0700

    created methods for getting results by division search

--- demo.py
+++ demo.py
@@ -5,17 +5,20 @@ from tournaments.tournament_scraping import create_tournament_data_from_url, com
 from tournaments.tournament_data import TournamentData
 from get_results import get_dataframes_from_tournament_url_list
 from get_results import get_url_list_from_seach
+from get_results import get_results_for_division
 from soup_scraping import get_search_params
 
-testing_single_tournament = False
-testing_list_tournaments = False
-testing_results_search = True
 
+testing_single_tournament = True
+testing_list_tournaments = False
+testing_results_search = False
+test_results_by_division = False
 
 if testing_single_tournament:
     print("\n\n Reading and printing a single tournament")
-    print(     "----------------------------------------\n\n")
+    print("----------------------------------------\n\n")
     tournament_url = 'https://fie.org/competitions/2020/771'
+    tournament_url = 'https://fie.org/competitions/2016/941'
     print("Tournament URL for lookup: {}".format(tournament_url))
     tournament = create_tournament_data_from_url(tournament_url)
     print(tournament)
@@ -25,17 +28,18 @@ if testing_single_tournament:
     bout_count = 1
     idx = random.sample(list(bout_dataframe.index), bout_count)
     print("A random bout from the tournament:\n".format(bout_count))
-    print(bout_dataframe.loc[idx].drop(columns=['opp_age','opp_curr_pts','date']).to_markdown())
+    print(bout_dataframe.loc[idx].drop(
+        columns=['opp_age', 'opp_curr_pts', 'date']).to_markdown())
 
     time.sleep(5)
 
 if testing_list_tournaments:
     print("\n\n Loading a list of tournaments, compiling bouts and fencer info")
-    print(     "----------------------------------------------------------------\n\n")
+    print("----------------------------------------------------------------\n\n")
 
     list_of_urls = ['https://fie.org/competitions/2021/1081']
-    tourn_df, bout_df, fencers_bio_df, fencers_rankings_df = get_dataframes_from_tournament_url_list(list_of_urls)
-
+    tourn_df, bout_df, fencers_bio_df, fencers_rankings_df = get_dataframes_from_tournament_url_list(
+        list_of_urls)
 
     print("\n\n")
     time.sleep(2)
@@ -64,11 +68,10 @@ if testing_list_tournaments:
     print("\nA random selection of {} fencers from rankings list:\n".format(fencer_count))
     print(fencers_rankings_df.loc[idx])
 
-
 if testing_results_search:
 
     print("\n\n Loading a list of tournaments url")
-    print(     "----------------------------------\n\n")
+    print("----------------------------------\n\n")
     print("\n\nGetting results for Women's Foil...\n")
 
     # search_params = get_search_params(weapon=['f'],gender=['f'])
@@ -76,8 +79,46 @@ if testing_results_search:
 
     print("\n\nGetting results for Cadet Women's Sabre...\n")
 
-    search_params = get_search_params(weapon=['s'],gender=['f'],category='c')
+    search_params = get_search_params(weapon=['s'], gender=['f'], category='c')
     url_list = get_url_list_from_seach(search_params)
 
     print(len(url_list))
     print(url_list[0:100])
+
+if test_results_by_division:
+    print("\n\n Loading all results + fencer data for a division")
+    print("----------------------------------------------------------------\n\n")
+
+    print("Getting results for Cadet Women's Sabre...\n")
+
+    tourn_df, bout_df, fencers_bio_df, fencers_rankings_df = get_results_for_division(
+        weapon=['s'], gender=['f'], category='c')
+
+    print("\n\n")
+    time.sleep(2)
+
+    # Print dataframes (or parts of them) to see output
+
+    tournament_count = 2
+    idx = random.sample(list(tourn_df.index), tournament_count)
+    print(tourn_df.loc[idx].drop(
+        columns=['timezone', 'url', 'end_date']).to_markdown())
+    # print(tourn_df.info())
+
+    bout_count = 3
+    idx = random.sample(list(bout_df.index), bout_count)
+    print("\nA random selection of bouts from list:\n".format(bout_count))
+    print(bout_df.loc[idx].drop(
+        columns=['opp_age', 'opp_curr_pts']).to_markdown())
+    # print(bout_df.info())
+
+    fencer_count = 5
+    idx = random.sample(list(fencers_bio_df.index), fencer_count)
+    print("\nA random selection of {} fencers from bio list:\n".format(fencer_count))
+    print(fencers_bio_df.loc[idx].to_markdown())
+
+    fencer_count = 2
+    idx = random.sample(
+        list(fencers_rankings_df.index.get_level_values(0)), fencer_count)
+    print("\nA random selection of {} fencers from rankings list:\n".format(fencer_count))
+    print(fencers_rankings_df.loc[idx])
--- get_results.py
+++ get_results.py
@@ -9,37 +9,35 @@ from dataframe_columns import multiIndex_relabeler, make_season_from_year
 from tournaments.tournament_scraping import create_tournament_data_from_url, compile_bout_dataframe_from_tournament_data
 from tournaments.tournament_data import TournamentData
 from fencers.fencer_scraping import get_fencer_dataframes_from_ID_list, convert_list_to_dataframe_with_multi_index
+from soup_scraping import get_search_params
 
 
 def add_tournament_urls_to_list(url_list, tournament_dict_list):
     for tournament in tournament_dict_list:
-        url = "https://fie.org/"+str(tournament['season'])+"/"+str(tournament['competitionId'])
-        print('Event URL: \'{}\''.format(url))
-        url_list.append(url) 
+        url = "https://fie.org/competitions/" + \
+            str(tournament['season'])+"/"+str(tournament['competitionId'])
+        url_list.append(url)
     return url_list
 
 
 def get_url_list_from_seach(search_params):
     url_list = []
 
-    # get first page of results and check if more pages needed 
+    # get first page of results and check if more pages needed
     search_params['fetchPage'] = 1
     search_url = 'https://fie.org/competitions/search'
     req = requests.post(search_url, data=search_params)
     json = req.json()
-    print("Total Events Found: {}".format(json['totalFound']))
     # json object sample in initial_testing/request_response.json
     pages_needed = math.ceil(json['totalFound']/json['pageSize'])
     url_list = add_tournament_urls_to_list(url_list, json['items'])
-    for p in range(2,pages_needed+1):
+    for p in range(2, pages_needed+1):
         search_params['fetchPage'] = p
         req = requests.post(search_url, data=search_params)
         json = req.json()
         url_list = add_tournament_urls_to_list(url_list, json['items'])
 
     return url_list
-
-
 
 
 def process_tournament_data_from_urls(list_of_urls):
@@ -50,23 +48,24 @@ def process_tournament_data_from_urls(list_of_urls):
 
     fencer_ID_list = []
 
-    print("Processing {} tournaments: ".format(len(list_of_urls)), end="")
+    # print("Processing {} tournaments: ".format(len(list_of_urls)), end="")
 
-    for tournament_url in Bar('Loading Tournaments').iter(list_of_urls):
+    for tournament_url in Bar('  Loading tournaments').iter(list_of_urls):
         # process data from the event
         tournament_data = create_tournament_data_from_url(tournament_url)
-        tournament_bout_dataframe = compile_bout_dataframe_from_tournament_data(
-            tournament_data)
-        tournament_info_dict = tournament_data.create_tournament_dict()
-        tournament_fencer_ID_list = list(tournament_data.fencers_dict.keys())
+        if tournament_data is not None:     
+            tournament_bout_dataframe = compile_bout_dataframe_from_tournament_data(
+                tournament_data)
+            tournament_info_dict = tournament_data.create_tournament_dict()
+            tournament_fencer_ID_list = list(tournament_data.fencers_dict.keys())
 
-        # add tournament data to overall dataframes/lists
-        fencer_ID_list = list(set(fencer_ID_list+tournament_fencer_ID_list))
-        bouts_dataframe = bouts_dataframe.append(tournament_bout_dataframe)
-        tournaments_dataframe = tournaments_dataframe.append(
-            tournament_info_dict, ignore_index=True)
-        # print("\rProcessing {} tournaments: {} tournaments done... ".format(
-        #     len(list_of_urls), idx+1), end="")
+            # add tournament data to overall dataframes/lists
+            fencer_ID_list = list(set(fencer_ID_list+tournament_fencer_ID_list))
+            bouts_dataframe = bouts_dataframe.append(tournament_bout_dataframe)
+            tournaments_dataframe = tournaments_dataframe.append(
+                tournament_info_dict, ignore_index=True)
+            # print("\rProcessing {} tournaments: {} tournaments done... ".format(
+            #     len(list_of_urls), idx+1), end="")
 
     return tournaments_dataframe, bouts_dataframe, fencer_ID_list
 
@@ -89,7 +88,7 @@ def cleanup_dataframes(tournaments_dataframe, bouts_dataframe,
         category_dict)
     fencers_bio_dataframe['hand'] = fencers_bio_dataframe['hand'].map(
         hand_dict)
-        
+
     multiIndex_relabeler(fencers_rankings_dataframe,
                          level=1, mapper=weapon_dict)
     multiIndex_relabeler(fencers_rankings_dataframe,
@@ -136,3 +135,14 @@ def get_dataframes_from_tournament_url_list(list_of_urls, fencer_cap=-1):
     print(" Done!")
 
     return tournaments_dataframe, bouts_dataframe, fencers_bio_dataframe, fencers_rankings_dataframe
+
+
+def get_results_for_division(weapon=[], gender=[], category=""):
+    search_params = get_search_params(weapon, gender, category)
+    url_list = get_url_list_from_seach(search_params)
+    mini_list = url_list[26:28]
+    print("Results search found {} tournaments ".format(len(url_list)))
+    print("  (processing a mini list for testing)")
+    tournament_df, bouts_df, fencer_bio_df, fencer_rank_df = get_dataframes_from_tournament_url_list(
+        mini_list)
+    return tournament_df, bouts_df, fencer_bio_df, fencer_rank_df
