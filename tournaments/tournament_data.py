f42bbe6193bb634a78d5f7fa27067ad1689c88d2
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-11 13:54:51 -0700

    autoformat

--- tournaments/tournament_data.py
+++ tournaments/tournament_data.py
@@ -26,21 +26,21 @@ class TournamentData:
         endDate       -> end_date
         competitionId -> competition_ID
     '''
-    unique_ID : str # this will be season-competition_id, e.g. "2020-771"
-    season : int
-    competition_ID : int
-    weapon : str  
-    gender : str 
-    category : str 
-    start_date : str
-    end_date : str
-    name : str 
-    country : str
-    timezone : str  
-    url : str 
+    unique_ID: str  # this will be season-competition_id, e.g. "2020-771"
+    season: int
+    competition_ID: int
+    weapon: str
+    gender: str
+    category: str
+    start_date: str
+    end_date: str
+    name: str
+    country: str
+    timezone: str
+    url: str
 
-    pools_list : list[PoolData]
-    fencers_dict : dict
+    pools_list: list[PoolData]
+    fencers_dict: dict
 
     def create_tournament_dict(self):
         tournament_dict = self.__dict__.copy()
@@ -50,22 +50,26 @@ class TournamentData:
 
     def __str__(self):
         str_rep = "\nTournament Information:\n"
-        str_rep +=   "-----------------------\n"
-        str_rep += "   Name:  {:<25} (unique ID {:<10})\n".format(self.name, self.unique_ID)
-        str_rep += "   Season:  {:<10}  Competition ID:  {:<5}\n".format(self.season, self.competition_ID)
-        str_rep += "   Weapon:  {:<10}  Gender:          {:<5}\n".format(self.weapon, self.gender)
-        str_rep += "   Country: {:<10}  TimeZone:        {:<5}\n".format(self.country, self.timezone)
-        str_rep += "   Start:   {:<10}  End:             {:<5}\n".format(self.start_date, self.end_date)
+        str_rep += "-----------------------\n"
+        str_rep += "   Name:  {:<25} (unique ID {:<10})\n".format(
+            self.name, self.unique_ID)
+        str_rep += "   Season:  {:<10}  Competition ID:  {:<5}\n".format(
+            self.season, self.competition_ID)
+        str_rep += "   Weapon:  {:<10}  Gender:          {:<5}\n".format(
+            self.weapon, self.gender)
+        str_rep += "   Country: {:<10}  TimeZone:        {:<5}\n".format(
+            self.country, self.timezone)
+        str_rep += "   Start:   {:<10}  End:             {:<5}\n".format(
+            self.start_date, self.end_date)
         str_rep += "   Website:   {} \n\n\n".format(self.url)
 
-
-        str_rep += "Total Number of Fencers: {}\n".format(len(self.fencers_dict.keys()))
+        str_rep += "Total Number of Fencers: {}\n".format(
+            len(self.fencers_dict.keys()))
         str_rep += "Total Number of Pools: {}".format(len(self.pools_list))
 
         str_rep += "\n\nPool Results:\n"
-        str_rep +=     "---------------\n\n"
+        str_rep += "---------------\n\n"
         for pool in self.pools_list:
             str_rep += pool.__str__()
 
         return str_rep
-    
