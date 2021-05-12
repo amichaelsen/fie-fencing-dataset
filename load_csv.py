731bdcf35c01dfa5543975f9c067a65d7afaa4ae
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-12 16:03:52 -0700

    updated load_csv to be more flexible and takes a date/div var

--- load_csv.py
+++ load_csv.py
@@ -1,10 +1,10 @@
 import pandas as pd 
 
 date = 'May_12_2021'
-div_name = 'cadet_womens_foil'
+div_name = 'all_womens_foil'
 
-bout_data            = pd.read_csv('output/'+date+'/'+div_name+ '_bout_data_'            +date+'.csv')
-tournament_data      = pd.read_csv('output/'+date+'/'+div_name+ '_tournament_data_'      +date+'.csv')
-fencer_bio_data      = pd.read_csv('output/'+date+'/'+div_name+ '_fencer_bio_data_'      +date+'.csv')
-fencer_rankings_data = pd.read_csv('output/'+date+'/'+div_name+ '_fencer_rankings_data_' +date+'.csv')
+bout_df            = pd.read_csv('output/'+date+'/'+div_name+ '_bout_data_'            +date+'.csv')
+tournament_df      = pd.read_csv('output/'+date+'/'+div_name+ '_tournament_data_'      +date+'.csv')
+fencer_bio_df      = pd.read_csv('output/'+date+'/'+div_name+ '_fencer_bio_data_'      +date+'.csv')
+fencer_rankings_df = pd.read_csv('output/'+date+'/'+div_name+ '_fencer_rankings_data_' +date+'.csv')
 
