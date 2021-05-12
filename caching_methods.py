e2baf54c35b80741187e33cb5925201293d9fa7c
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-12 14:53:54 -0700

    more methods for loading information from cache directly

--- caching_methods.py
+++ caching_methods.py
@@ -1,5 +1,9 @@
 from os import path, stat
 import json
+import pandas as pd
+from dataframe_columns import FENCERS_BIO_DF_COLS, FENCERS_RANKINGS_DF_COLS, FENCERS_RANKINGS_MULTI_INDEX
+from dataframe_columns import convert_list_to_dataframe_with_multi_index
+from progress.bar import Bar
 
 
 def save_dict_to_cache(cache_filename, dict_key, dict_value):
@@ -27,3 +31,38 @@ def clear_null_entries(cache_filename):
                 del cached_data[key]
         with open(cache_filename, 'w') as write_file:
             json.dump(cached_data, write_file)
+
+
+def load_all_cached_fencers_bio():
+    cache_filename = 'fencers/fencer_cache.txt'
+    with open(cache_filename) as read_file:
+        cached_data = json.load(read_file)
+        print(type(cached_data.values()))
+    fencers_bio_dataframe = pd.DataFrame(
+        data=cached_data.values(), columns=FENCERS_BIO_DF_COLS)
+    return fencers_bio_dataframe
+
+
+def load_all_cached_fencers_rankings():
+    cache_filename = 'fencers/fencer_cache.txt'
+    fencer_rankings_list = []
+    with open(cache_filename) as read_file:
+        cached_data = json.load(read_file)
+        print(type(cached_data.values()))
+        for value in cached_data.values():
+            fencer_rankings_list = fencer_rankings_list + value['rankings']
+    fencers_rankings_df = convert_list_to_dataframe_with_multi_index(
+        list_of_results=fencer_rankings_list,
+        column_names=FENCERS_RANKINGS_DF_COLS, index_names=FENCERS_RANKINGS_MULTI_INDEX)
+    return fencers_rankings_df
+
+
+def get_tournament_from_fencer(fencer_ID):
+    cache_filename = 'tournaments/tournament_cache.txt'
+    with open(cache_filename) as read_file:
+        cached_data = json.load(read_file)
+    fencer_tournaments = []
+    for tourn_id, tourn_data in Bar('Checking tournaments ').iter(cached_data.items()):
+        if fencer_ID in tourn_data['fencer_list']:
+            fencer_tournaments.append(tourn_data['unique_ID'])
+    return fencer_tournaments
