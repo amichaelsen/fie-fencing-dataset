363eae2f6314e5c5aaa9a70d2964ed515fcca6e4
amichaelsen <anya.michaelsen2@gmail.com>
2021-05-14 08:50:45 -0700

    added backup saving to main storing results in a temp location

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from os import path, makedirs
 
 from helper.get_results import get_results_for_division
 
-save_results = False 
+save_results = True 
 
 weapon = 'f'
-gender = 'm'
+gender = 'f'
 category = ''
 
 
@@ -22,12 +22,29 @@ print("----------------------------------------------------------------------\n\
 
 
 tourn_df, bout_df, fencers_bio_df, fencers_rankings_df = get_results_for_division(
-    weapon=[weapon], gender=[gender], category=category, max_events= 5, use_tournament_cache=False, use_fencer_data_cache=True, use_fencer_req_cache=True)
+    weapon=[weapon], gender=[gender], category=category, use_tournament_cache=True, use_fencer_data_cache=True, use_fencer_req_cache=True)
 
 if save_results:
 
     date_string = date.today().strftime("%b_%d_%Y")
     directory = 'output/'+date_string+"/"
+
+    if not path.exists(directory):
+        makedirs(directory)
+
+    name_list = ['tournament_data', 'bout_data', 'fencer_bio_data', 'fencer_rankings_data']
+    for idx, df in enumerate([tourn_df, bout_df, fencers_bio_df]):
+        name = name_list[idx]
+        file_name = div_name + "_" + name + "_" + date_string + ".csv"
+        df.to_csv(directory+file_name, index=False)
+    
+    name = 'fencer_rankings_data'
+    file_name = div_name + "_" + name + "_" + date_string + ".csv"
+    fencers_rankings_df.to_csv(directory+file_name)
+
+else: # store in a temp cache just in case! 
+    date_string = date.today().strftime("%b_%d_%Y")
+    directory = 'temp_cache/'+date_string+"/"
 
     if not path.exists(directory):
         makedirs(directory)
